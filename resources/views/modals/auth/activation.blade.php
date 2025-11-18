92fa732283b8096380a26f36088431dafcd46ef8
Ivars <ia@esteriol.lv>
2025-11-18 14:33:55 +0200

    activation returnword fixes

--- resources/views/modals/auth/activation.blade.php
+++ resources/views/modals/auth/activation.blade.php
@@ -1,34 +1,37 @@
 <a href="#" class="remodal-close" data-modal-close></a>
 
 <div class="remodal__body">
-    <form class="form js-activation-code-form" data-form="activation-code" method="post" action="#" onsubmit="return false;">
+    <form class="form js-activation-code-form" data-form="activation-code" method="post" action="#"
+          onsubmit="return false;">
         @csrf
         <input type="hidden" name="client_mail" id="client_mail"
                value="{{ e($mail) }}">
-        <div class="block__title js-title"><strong><?php getWord('Activation', WORDS_PROJECT); ?></strong></div>
+        <div class="block__title js-title"><strong>{!! returnWord('Activation', WORDS_INTERFACE) !!}</strong></div>
 
         <div class="js-alert alert" data-form="activation-code" data-group="default" style="display: none"></div>
 
         <div class="form__block">
-            <label for="activation-code" class="label"><?php getword("Activation code", WORDS_INTERFACE); ?>:</label>
+            <label for="activation-code" class="label">{!! returnWord('Activation code', WORDS_INTERFACE) !!}:</label>
             <input type="text" name="act_code" id="activation-code" class="input"
                    autocomplete="off">
         </div>
 
         <div class="form__block">
             <div id="countdown-wrapper">
-                <?php getWord('New code available after', WORDS_PROJECT) ?>&nbsp;<span
-                        id="countdown">{{ $seconds }}</span>&nbsp;<?php getword("s.", WORDS_INTERFACE); ?>
+                {!! returnWord('New code available after', WORDS_INTERFACE) !!}&nbsp;<span
+                    id="countdown">{{ $seconds }}</span>&nbsp;{!! returnWord('s.', WORDS_INTERFACE) !!}
             </div>
         </div>
 
         <div class="form__block js-resend-wrapper" style="display: none;">
-            <?= returnWord('Have not received activation code?', WORDS_INTERFACE) ?>
-            <a href="#" class="js-resend-button"><?= returnWord('Resend', WORDS_INTERFACE) ?></a>
+            {!! returnWord('Have not received activation code?', WORDS_INTERFACE) !!}
+            <a href="#" class="js-resend-button">{!! returnWord('Resend', WORDS_INTERFACE) !!}</a>
         </div>
 
         <div class="form__block">
-            <?php submitButton(['text' => returnWord('Submit', WORDS_INTERFACE)]); ?>
+            <button type="submit" class="btn btn--main">
+                {!! returnWord('Submit', WORDS_INTERFACE) !!}
+            </button>
         </div>
     </form>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,7 @@ Route::post('/modal/{action}', ModalContentController::class)
     ->name('modal-content');
 
 Route::post('/auth/login', LoginController::class)->name('auth.login');
-Route::post('/modules/auth/login/handler.php', LoginController::class)->name('legacy.auth.login');
 Route::post('/auth/activation', ActivationController::class)->name('auth.activation');
-Route::post('/modules/auth/activation/handler.php', ActivationController::class)->name('legacy.auth.activation');
 Route::post('/auth/activation/resend', ResendActivationCodeController::class)->name('auth.activation.resend');
-Route::post('/modules/auth/ajax/resend_activation_code.php', ResendActivationCodeController::class)->name('legacy.auth.activation.resend');
 
 Route::any('{any}', [PageController::class, 'handle'])->where('any', '.*');
